b36a0de6c9a7dbc0143e8b4986e7860a5e6d3aa8
reftch <j.developer@hotmail.com>
2026-06-13 07:21:07 +0200

    Bump version of http-server

--- src/main.cc
+++ src/main.cc
@@ -1,26 +1,25 @@
-#define CONNECTION_TIMEOUT_SECOND -1
 #include "server.h"
 
 int main() {
     static auto& log = http::Logger::getInstance();
 
-    auto host = GetEnv<std::string>("HOST", "0.0.0.0");
-    auto port = GetEnv<int>("PORT", 8080);
+    auto host = getEnv<std::string>("HOST", "0.0.0.0");
+    auto port = getEnv<int>("PORT", 8080);
 
     // Server instance
     http::Server s(host, port);
     // Register signal handler with capture
     static auto s_ptr = &s;
     signal(SIGINT, [](int) {
-        s_ptr->Stop();
+        s_ptr->stop();
     });
 
-    s.SetRoute<http::HttpMethod::GET>("/", [](const http::Request&, http::Response& res) {
-        auto dev_mode = GetEnv<bool>("DEV_MODE", false);
-        res.SetContent<http::ContentType::HTML>(dev_mode ? "index-dev.html" : "index.html");
+    s.setRoute<http::HttpMethod::GET>("/", [](const http::Request&, http::Response& res) {
+        auto dev_mode = getEnv<bool>("DEV_MODE", false);
+        res.setContent<http::ContentType::HTML>(dev_mode ? "index-dev.html" : "index.html");
     });
 
-    s.Start();
+    s.start();
 
     return 0;
 }
